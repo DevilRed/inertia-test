fa5d4eb920fe69f7af10195ee6d8c2a3dc1d4786
Christian <chrisdaq@gmail.com>
2024-05-31 15:52:23 -0400

    send Inertia response from HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\Todo;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('Home', [
+            'todos' => Todo::latest()->get(),
+        ]);
+    }
 }
