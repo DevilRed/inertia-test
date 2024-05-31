d7a61a0b6f04a921910c9dc3d8239dadb8d70105
Christian <chrisdaq@gmail.com>
2024-05-31 17:53:34 -0400

    add delete button on Todo component

--- resources/js/Components/Todo.vue
+++ resources/js/Components/Todo.vue
@@ -8,6 +8,14 @@
         />
 
         <p>{{ todo.task }}</p>
+
+        <button
+            type="button"
+            @click="deleteTodo"
+            class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+            X
+        </button>
     </div>
 </template>
 
@@ -29,4 +37,9 @@
     const save = () => {
         todoForm.put(`/todos/${ props.todo.id }`);
     };
+
+    // delete a todo
+    const deleteTodo = () => {
+        todoForm.delete(`/todos/${ props.todo.id }`);
+    }
 </script>
