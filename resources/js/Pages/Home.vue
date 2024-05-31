280b7ed00c00df71a05afffa8de8c352431905c3
Christian <chrisdaq@gmail.com>
2024-05-31 15:54:25 -0400

    load todos on home vue page

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,3 +1,21 @@
 <template>
-    <p>Hello, from Vue!</p>
+    <div class="mx-auto max-w-3xl mt-10 px-4">
+        <h1 class="font-light text-3xl">My Todo List</h1>
+
+        <div class="flex flex-col space-y-4 mt-5">
+            <Todo
+                v-for="todo in todos"
+                :key="todo.id"
+                :todo="todo"
+            />
+        </div>
+    </div>
 </template>
+
+<script setup>
+    import Todo from "../Components/Todo.vue";
+
+    defineProps([
+        'todos',
+    ]);
+</script>
